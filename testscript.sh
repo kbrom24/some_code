5c0ea90273af48ad1dc1f5b65e7e189a2d47f7c4
kbrom24 <kbrom24@gmail.com>
2019-01-22 13:54:10 -0600

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,6 +3,6 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "i have a commit"
-
+echo "edit the file"
 echo "this was run automagically"
 echo "my name is blue"
